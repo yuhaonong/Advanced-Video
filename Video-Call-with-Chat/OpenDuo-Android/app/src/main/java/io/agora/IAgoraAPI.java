3aa8863056851f46e51d97d0feb2189be387f572
Haonong Yu <yuhaonong@agora.io>
2018-11-29 23:47:19 +0800

    add comments

--- Video-Call-with-Chat/OpenDuo-Android/app/src/main/java/io/agora/IAgoraAPI.java
+++ Video-Call-with-Chat/OpenDuo-Android/app/src/main/java/io/agora/IAgoraAPI.java
@@ -2,7 +2,7 @@ package io.agora;
 
 public interface IAgoraAPI {
     /**
-     * Commands
+     * signals
      */
     String SIGNAL_PREFIX = "SIG_";
     String SIG_QUERY_USER_STATUS = SIGNAL_PREFIX + "QUERY_USER_STATUS";
@@ -17,7 +17,20 @@ public interface IAgoraAPI {
      * @param handler A handler of ICallBack
      */
     void callbackSet(ICallBack handler);
+
+    /**
+     * Log into Agora's Signaling System. Users must always log in before performing any operation.
+     * @param appId The App ID provided by Agora
+     * @param account User ID defined by the client. It can be up to 64 visible characters (space and "null" is not allowed).
+     * @param token
+     * @param uid N/A: Set it as 0
+     * @param deviceID Set it as NULL
+     */
     void login(String appId, String account, String token, int uid, String deviceID);
+
+    /**
+     * Log out of the Agora's Signaling System.
+     */
     void logout();
 
     void queryUserStatus(String account);
@@ -26,7 +39,6 @@ public interface IAgoraAPI {
     void channelInviteAccept(String channelID, String account, int uid, String extra);
     void channelInviteRefuse(String channelID, String account, int uid,  String extra);
     void channelInviteEnd(String channelID, String account, int uid);
-
 
     interface ICallBack {
 
